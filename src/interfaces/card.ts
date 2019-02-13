583a418a7cf662f850ce96535e96f02f4ca6ac4f
Yusuke Sato <yusuke1.sato9.git@gmail.com>
2019-02-13 23:13:32 +0900

    give type to the return value of selectors

--- src/interfaces/card.ts
+++ src/interfaces/card.ts
@@ -1,11 +1,19 @@
 import { ApiError, PageInfo } from './GithubAPI';
 
+export interface FeedIndexByQuery {
+  [query: string]: Feed;
+}
+
 export interface Feed {
   pageInfo: PageInfo;
   query: string;
   cards: Card[];
   isFeatching: boolean;
   error: ApiError;
+}
+
+export interface CardsIndexByQuery {
+  [query: string]: Card[];
 }
 
 export interface Card {
--- src/state/queryBrowser/selectors.ts
+++ src/state/queryBrowser/selectors.ts
@@ -1,48 +1,47 @@
 import { isUndefined } from 'lodash-es';
 import { createSelector } from 'reselect';
-import { Card, Feed } from '../../interfaces/card';
+import { CardsIndexByQuery, FeedIndexByQuery } from '../../interfaces/card';
 import { RootState } from '../store';
 
 const getQueryBrowserFromStore = (store: RootState) => store.queryBrowser;
 
 const getCardsByQuery = createSelector(
   getQueryBrowserFromStore,
-  queryBrowser =>
-    Object.keys(queryBrowser).reduce(
-      (acc: { [query: string]: Card[] }, query) => {
-        const { result } = queryBrowser[query];
-        return isUndefined(result)
-          ? acc
-          : {
-              ...acc,
-              [query]: result.edges.map(edge => edge.node),
-            };
-      },
-      {},
-    ),
+  queryBrowser => {
+    const initialValue: CardsIndexByQuery = {};
+    return Object.keys(queryBrowser).reduce((acc: CardsIndexByQuery, query) => {
+      const { result } = queryBrowser[query];
+      return isUndefined(result)
+        ? acc
+        : {
+            ...acc,
+            [query]: result.edges.map(edge => edge.node),
+          };
+    }, initialValue);
+  },
 );
 
 const getFeedByQuery = createSelector(
   getQueryBrowserFromStore,
   getCardsByQuery,
-  (queryBrowser, cardsByQuery) =>
-    Object.keys(queryBrowser).reduce(
-      (acc: { [query: string]: Feed }, query) => {
-        const { result, isFeatching, error } = queryBrowser[query];
-        return isUndefined(result)
-          ? acc
-          : {
-              ...acc,
-              [query]: {
-                error,
-                isFeatching,
-                cards: cardsByQuery[query],
-                pageInfo: result.pageInfo,
-              },
-            };
-      },
-      {},
-    ),
+  (queryBrowser, cardsByQuery) => {
+    const initialValue: FeedIndexByQuery = {};
+    return Object.keys(queryBrowser).reduce((acc: FeedIndexByQuery, query) => {
+      const { result, isFeatching, error } = queryBrowser[query];
+      return isUndefined(result)
+        ? acc
+        : {
+            ...acc,
+            [query]: {
+              query,
+              error,
+              isFeatching,
+              cards: cardsByQuery[query],
+              pageInfo: result.pageInfo,
+            },
+          };
+    }, initialValue);
+  },
 );
 
 export default {
