ca58b74b3a10011f784889cc1f339f71ce5724bd
Yusuke Sato <yusuke1.sato9.git@gmail.com>
2019-03-21 23:33:03 +0900

    install enzyme

---
+++ jest.config.js
@@ -0,0 +1,45 @@
+module.exports = {
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.d.ts'],
+  resolver: 'jest-pnp-resolver',
+  setupFiles: [
+    'jest-canvas-mock',
+    'react-app-polyfill/jsdom',
+    '<rootDir>/config/enzyme.config.js',
+  ],
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
+    '<rootDir>/src/**/?(*.)(spec|test).{js,jsx,ts,tsx}',
+  ],
+  testEnvironment: 'jsdom',
+  testURL: 'http://localhost',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+    '^.+\\.css$': '<rootDir>/config/jest/cssTransform.js',
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)':
+      '<rootDir>/config/jest/fileTransform.js',
+  },
+  transformIgnorePatterns: [
+    '[/\\\\]node_modules[/\\\\](?!(lodash-es|react-native-elements|react-native-vector-icons|react-native-status-bar-height|react-native-ratings)).+\\.(js|jsx|ts|tsx)$',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+  moduleNameMapper: {
+    '^react-native$': 'react-native-web',
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+  },
+  moduleFileExtensions: [
+    'web.js',
+    'js',
+    'web.ts',
+    'ts',
+    'web.tsx',
+    'tsx',
+    'json',
+    'web.jsx',
+    'jsx',
+    'node',
+  ],
+  watchPlugins: [
+    '/Users/yusuke_sato/.ghq/github.com/sat0yu/ghz/node_modules/jest-watch-typeahead/filename.js',
+    '/Users/yusuke_sato/.ghq/github.com/sat0yu/ghz/node_modules/jest-watch-typeahead/testname.js',
+  ],
+};
